9009d4b827f0049806cd0f80ce2160c4947f3433
Boris Sulicenko <b.sulicenko@gmail.com>
2020-05-06 14:39:03 +0200

    Osnovno pretprocesiranje teksta

--- domaci03/zadatak03.py
+++ domaci03/zadatak03.py
@@ -1,10 +1,14 @@
 import sys
 import json
+import string
+
+stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 class Article:
     def __init__(self, clickbait, text):
         self.clickbait = clickbait
         self.text = text
+        self.words = []
 
 def load_from_file(file_path):
     with open(file_path) as data_file:
@@ -14,7 +18,21 @@ def load_from_file(file_path):
         for d in data:
             articles.append(Article(d['clickbait'], d['text']))
 
-        return data
+        return articles
+
+def preprocess_text(data):
+    for article in data:
+        # prebaci sve na mala slova
+        article.text = article.text.lower()
+
+        # obrisi znakove interpunkcije
+        article.text = article.text.translate(str.maketrans('', '', string.punctuation))
+
+        # izbaci stopwords
+        words = article.text.split(' ')
+        article.words = [word for word in words if not(word in stopwords)]
+
+        #print(article.words)
 
 if __name__ == "__main__":
     train_path = sys.argv[1]
@@ -22,3 +40,6 @@ if __name__ == "__main__":
 
     train_data = load_from_file(train_path)
     test_data = load_from_file(test_path)
+
+    preprocess_text(train_data)
+    preprocess_text(test_data)
